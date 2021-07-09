897863681644c94b3725e94b79d2e65eeaf5431b
stefon <s.j.simmons@spartans.nsu.edu>
2021-07-09 13:11:09 -0400

    added index.html and working refresh token method

--- create_playlist.py
+++ create_playlist.py
@@ -1,4 +1,5 @@
 import requests
+import webbrowser
 import json
 import sys
 import base64
@@ -7,40 +8,45 @@ from secrets import user_id, client_id, client_secret
 class CreatePlaylist:
 
     def __init__(self):
+        self.playlist_id = ''
         self.user_id = user_id
         self.token = ""
-        self.playlist_id = ""
         self.song_uri = ""
 
     # create a playlist on my account 
     def create_playlist(self, name, description, public=False):
+        self.get_new_token()
+        
         data = json.dumps({
             "name": name,
             "description": description,
             "public": public
         })
         url = "https://api.spotify.com/v1/users/{}/playlists".format(self.user_id)
+        print(CreatePlaylist.token)
         headers = {
             "Content-Type": "application/json",
-            "Authorization": "Bearer {}".format(self.token)
+            "Authorization": "Bearer {}".format(CreatePlaylist.token)
         }
         try:
             res = requests.post(url, data=data, headers=headers)
             res_json = res.json()
-            self.playlist_id = res_json["id"]
-            print("Playlist_ID: ", res_json["id"])
-            return res_json["id"]
+            print(res_json)
+            # self.playlist_id = res_json["id"]
+            # print("Playlist_ID: ", res_json["id"])
+            # return res_json["id"]
         except Exception as e: 
-            print("ERROR ",e)
+            print("Create Playlist ERROR ",e)
 
     #  search for a song on spotify
     def search_spotify(self,query):
         self.get_new_token()
         
         url = f"https://api.spotify.com/v1/search?q={query}&type=track&market=us&limit=1"
+        print(CreatePlaylist.token)
         headers = {
             "Content-Type": "application/json",
-            "Authorization": "Bearer {}".format(self.token)
+            "Authorization": "Bearer {}".format(CreatePlaylist.token)
         }
 
         try:
@@ -64,10 +70,14 @@ class CreatePlaylist:
             "Content-Type": "application/json",
             "Authorization": "Bearer {}".format(self.token)
         }
-        res = requests.post(url, data=data, headers=headers)
-        print(res.json())
+        try:
+            res = requests.post(url, data=data, headers=headers)
+            print(res.json())
+        except Exception as e:
+            print("Add Song ERROR: ", e)
         
-    # refresh OAuth Token - https://developer.spotify.com/documentation/general/guides/authorization-guide/#client-credentials-flow
+    # refresh OAuth Token - 
+    # client flow - https://developer.spotify.com/documentation/general/guides/authorization-guide/#client-credentials-flow
     def get_new_token(self):
         client_cred=client_id+":"+client_secret
         encoded_bytes = base64.b64encode(client_cred.encode("utf-8")) #base64 encoded client_id:client_secret. https://www.base64encoder.io/python/
@@ -79,20 +89,35 @@ class CreatePlaylist:
         url = "https://accounts.spotify.com/api/token/"
         try:
             res = requests.post(url, data="grant_type=client_credentials", headers=headers)
-            self.token = res.json()["access_token"]
+            res_json = res.json()
+            CreatePlaylist.token = res_json["access_token"]
         except Exception as e:
-            print("TOKEN ERROR: ", e)
+            print("Get Token ERROR: ", e)
 
+    # authorization flow - https://developer.spotify.com/documentation/general/guides/authorization-guide/#authorization-code-flow
+    def refresh_token(self):
+        client_query = "client_id={}".format(client_id)
+        response_query = "response_type=code"
+        redirect_uri = "redirect_uri=https://stefonsimmons.me/about"
+        scopes = "scope=user-read-private"
+        url = "https://accounts.spotify.com/authorize?{}&{}&{}&{}".format(client_query, response_query, redirect_uri, scopes)
+        
+        try:
+            res = requests.get(url)
+            webbrowser.open(res.url)
+            print(res.url)
+        except Exception as e:  
+            print('Refresh: ', e)
 
 new_list = CreatePlaylist()
 
-if sys.argv[1] == "cp":
-    description = ' '.join(sys.argv[3:])
-    new_list.create_playlist(sys.argv[2], description)
-elif sys.argv[1] == "s":
-    query = ' '.join(sys.argv[2:])
-    new_list.search_spotify(query)
+# if sys.argv[1] == "cp":
+#     description = ' '.join(sys.argv[3:])
+#     new_list.create_playlist(sys.argv[2], description)
+# elif sys.argv[1] == "s":
+#     query = ' '.join(sys.argv[2:])
+#     new_list.search_spotify(query)
 
 # new_list.add_song_to_playlist()
-
-print(new_list.playlist_id)
+# print(new_list.playlist_id)
+new_list.refresh_token()

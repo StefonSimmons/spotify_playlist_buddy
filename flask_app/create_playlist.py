3b4f59619ccd0d326cd74a6fdb035fc6a32b6d87
stefon <s.j.simmons@spartans.nsu.edu>
2021-07-10 12:40:42 -0400

    public vs private code fix

--- flask_app/create_playlist.py
+++ flask_app/create_playlist.py
@@ -10,8 +10,8 @@ class Playlist:
 
     def __init__(self):
         self.token = ""
-        self.user_id = user_id
         self.song_uri = ""
+        self.redirect_uri = "https://spotify-playlist-buddy.vercel.app/"
 
     # create a playlist on my account 
     def create_playlist(self, name, description, public):    
@@ -20,7 +20,7 @@ class Playlist:
             "description": description,
             "public": public
         })
-        url = "https://api.spotify.com/v1/users/{}/playlists".format(self.user_id)
+        url = "https://api.spotify.com/v1/users/{}/playlists".format(user_id)
         headers = {
             "Content-Type": "application/json",
             "Authorization": "Bearer {}".format(self.token)
@@ -34,14 +34,12 @@ class Playlist:
 
     #  search for a song on spotify
     def search_spotify(self,query):
-        self.get_client_flow_token()
-        
+        self.get_client_flow_token()  
         url = f"https://api.spotify.com/v1/search?q={query}&type=track&market=us&limit=1"
         headers = {
             "Content-Type": "application/json",
             "Authorization": "Bearer {}".format(self.token)
         }
-
         try:
             res = requests.get(url, headers=headers)
             res_json = res.json()
@@ -94,6 +92,7 @@ class Playlist:
         except Exception as e:
             print("Get Playlists Error: ", e)
 
+    # play user's spotify player on running device
     def play(self):
         url = "https://api.spotify.com/v1/me/player/play"
         headers={
@@ -105,6 +104,7 @@ class Playlist:
         except Exception as e:
             print("PLAY Error: ", e)
 
+    # pause user's spotify player on running device
     def pause(self):
         url = "https://api.spotify.com/v1/me/player/pause"
         headers={
@@ -138,8 +138,6 @@ class Playlist:
     # get OAuth Token for User Acct Access- https://developer.spotify.com/documentation/general/guides/authorization-guide/#client-credentials-flow
     def get_user_auth_token(self,refresh_code):
         client_cred=client_id+":"+client_secret
-        print("REFRESH: ", refresh_code)
-        print("ID ",client_id," SECRET ", client_secret)
         encoded_bytes = base64.b64encode(client_cred.encode("utf-8")) #base64 encoded client_id:client_secret. https://www.base64encoder.io/python/
         encoded_client = str(encoded_bytes, "utf-8")
         headers = {
@@ -147,11 +145,10 @@ class Playlist:
             "Authorization": "Basic {}".format(encoded_client)
         }
         url = "https://accounts.spotify.com/api/token/"
-        data = "grant_type=authorization_code&code={}&redirect_uri=https://spotify-playlist-buddy.vercel.app/".format(refresh_code)
+        data = "grant_type=authorization_code&code={}&redirect_uri={}".format(refresh_code, self.redirect_uri)
         try:
             res = requests.post(url, data=data, headers=headers)
             res_json = res.json()
-            print("RES ",res_json)
             self.token = res_json["access_token"]
         except Exception as e:
             print("Get Token ERROR: ", e)
@@ -160,7 +157,7 @@ class Playlist:
     def get_user_approval(self):
         client_query = "client_id={}".format(client_id)
         response_query = "response_type=code"
-        redirect_uri = "redirect_uri=https://spotify-playlist-buddy.vercel.app/"
+        redirect_uri = "redirect_uri={}".format(self.redirect_uri)
         scopes = "scope=user-read-private playlist-modify-private playlist-modify-public playlist-read-private user-modify-playback-state"
         url = "https://accounts.spotify.com/authorize?{}&{}&{}&{}".format(client_query, response_query, redirect_uri, scopes)
         
@@ -180,11 +177,11 @@ def run_user_auth_process():
 
 if sys.argv[1] == "cp":
     description = ' '.join(sys.argv[3:])
-    private = input("Is {} Public? (Y)".format(sys.argv[2]))
-    if private == "Y": private = True
-    else: private = False
+    public = input("Is {} Public? (Y)".format(sys.argv[2]))
+    if public == "Y": public = True
+    else: public = False
     run_user_auth_process()
-    new_list.create_playlist(sys.argv[2], description, private)
+    new_list.create_playlist(sys.argv[2], description, public)
 elif sys.argv[1] == "s":
     query = ' '.join(sys.argv[2:])
     new_list.search_spotify(query)
